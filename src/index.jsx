d9b134eb3bc4983747827c55ba1b4aaa22d46bd4
Никита Вариводин <58493296+h-imko@users.noreply.github.com>
2025-09-02 01:04:23 +0300

    /

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import Page from "./components/page"
 export default function index() {
 	return (
 		<Page>
-			<div className="container">
-				<div className="cloud"></div>
+			<div className="cloud">
+				<div className="cloud__words"> </div>
 			</div>
 		</Page>
 	)
