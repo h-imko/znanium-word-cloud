ba51ed1fdbcf31551fb2e6113affeb276260f63d
Никита Вариводин <58493296+h-imko@users.noreply.github.com>
2025-09-12 12:21:46 +0300

    /

--- src/alternative.jsx
+++ src/alternative.jsx
@@ -4,7 +4,7 @@ export default function index() {
   return (
     <Page>
       <a href="index.html" style="font-size: 48px;">С перспективой</a> <br />
-      <a href="alternative.1.html" style="font-size: 48px;">Без перспективы и без затухания</a>
+      <a href="alternative.1.html" style="font-size: 48px;">С перспективой и без затухания</a>
       <div className="cloud cloud--1">
         <div className="cloud__words"> </div>
       </div>
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ export default function index() {
 	return (
 		<Page>
 			<a href="alternative.html" style="font-size: 48px;">Без перспективы</a> <br />
-			<a href="alternative.1.html" style="font-size: 48px;">Без перспективы и без затухания</a>
+			<a href="alternative.1.html" style="font-size: 48px;">С перспективой и без затухания</a>
 			<div className="cloud">
 				<div className="cloud__words"> </div>
 			</div>
